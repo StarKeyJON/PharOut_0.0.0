b5c1c0320ab2442c120146886d03fdb193c2aab8
Ken Preiser <kenpreiser@gmail.com>
2022-03-03 11:06:26 -0800

    Amend

---
+++ MarketCollections.sol
@@ -0,0 +1,167 @@
+//*~~~> SPDX-License-Identifier: MIT OR Apache-2.0
+/*~~~>
+    Thank you Phunks, your inspiration and phriendship meant the world to me and helped me through hard times.
+      Never stop phighting, never surrender, always stand up for what is right and make the best of all situations towards all people.
+      Phunks are phreedom phighters!
+        "When the power of love overcomes the love of power the world will know peace." - Jimi Hendrix <3
+        On the point of halting the trading of collections,
+          We the People of web3 must moderate ourselves. 
+          With any power comes great responsibility,
+           and if we don't moderate ourselves from within, 
+           then we will be moderated from without. 
+           I will always stand against CP or any form of profiteering from child exploitation 
+           and refuse to allow the trading or promotion of any material that is knowingly harmful or explicit.
+           Knowing this power can be controlled as a weapon against other projects, 
+            let us only refuse collections that are explicitly tied to CP or knowingly harmful activities.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((%%%%%
+%%%%%((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((%%%%%
+%%%%%((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((%%%%%
+%%%%%((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((%%%%%
+%%%%%((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((%%%%%
+%%%%%((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((%%%%%
+%%%%%((((((((((((((((((((((((((((((((((((((((@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@(((((((((((((((((((((((((((((((((((%%%%%
+%%%%%((((((((((((((((((((((((((((((((((((((((@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@(((((((((((((((((((((((((((((((((((%%%%%
+%%%%%(((((((((((((((((((((((((((((((((((@@@@@##############################%%%%%@@@@@((((((((((((((((((((((((((((((%%%%%
+%%%%%(((((((((((((((((((((((((((((((((((@@@@@##############################%%%%%@@@@@((((((((((((((((((((((((((((((%%%%%
+%%%%%((((((((((((((((((((((((((((((@@@@@########################################%%%%%@@@@@(((((((((((((((((((((((((%%%%%
+%%%%%((((((((((((((((((((((((((((((@@@@@########################################%%%%%@@@@@(((((((((((((((((((((((((%%%%%
+%%%%%(((((((((((((((((((((((((@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@###############@@@@@(((((((((((((((((((((((((%%%%%
+%%%%%(((((((((((((((((((((((((@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@###############@@@@@(((((((((((((((((((((((((%%%%%
+%%%%%((((((((((((((((((((@@@@@%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%@@@@@##########@@@@@(((((((((((((((((((((((((%%%%%
+%%%%%((((((((((((((((((((@@@@@%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%@@@@@##########@@@@@(((((((((((((((((((((((((%%%%%
+%%%%%((((((((((((((((((((@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@(((((((((((((((((((((((((%%%%%
+%%%%%((((((((((((((((((((@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@(((((((((((((((((((((((((%%%%%
+%%%%%(((((((((((((((SMOKE((((((((((@@@@@/////////////////////////////////////////////@@@@@(((((((((((((((((((((((((%%%%%
+%%%%%(((((((((((((((SMOKE((((((((((@@@@@/////////////////////////////////////////////@@@@@(((((((((((((((((((((((((%%%%%
+%%%%%(((((((((((((((SMOKE((((((((((@@@@@#PHUNKYJON///////////////#PHUNKYJON//////////@@@@@(((((((((((((((((((((((((%%%%%
+%%%%%(((((((((((((((SMOKE((((((((((@@@@@#PHUNKYJON///////////////#PHUNKYJON//////////@@@@@(((((((((((((((((((((((((%%%%%
+%%%%%(((((((((((((((SMOKE((((((((((@@@@@/////@EYES////////////////////@EYES///////////////@@@@@((((((((((((((((((((%%%%%
+%%%%%(((((((((((((((SMOKE((((((((((@@@@@/////@EYES////////////////////@EYES///////////////EAR@@((((((((((((((((((((%%%%%
+%%%%%(((((((((((((((SMOKE((((((((((@@@@@//////////////////////////////////////////////////EAR@@((((((((((((((((((((%%%%%
+%%%%%(((((((((((((((SMOKE((((((((((@@@@@//////////////////////////////////////////////////EAR@@((((((((((((((((((((%%%%%
+%%%%%(((((((((((((((SMOKE((((((((((@@@@@/////////////////////////////////////////////@@@@@@@@@@((((((((((((((((((((%%%%%
+%%%%%(((((((((((((((SMOKE((((((((((@@@@@/////////////////////////////////////////////@@@@@@@@@@((((((((((((((((((((%%%%%
+%%%%%(((((((((((((((SMOKE((((((((((@@@@@//////////NOSE@NOSE@////////////////////#####@@@@@(((((((((((((((((((((((((%%%%%
+%%%%%(((((((((((((((SMOKE((((((((((@@@@@//////////NOSE@NOSE@////////////////////#####@@@@@(((((((((((((((((((((((((%%%%%
+%%%%%((((((((((((((((((((((((((((((@@@@@#####//////////////////////////////##########@@@@@(((((((((((((((((((((((((%%%%%
+%%%%%((((((((((((((((((((((((((((((@@@@@#####//////////////////////////////##########@@@@@(((((((((((((((((((((((((%%%%%
+%%%%%(((((((((((((((@SPLIFF@SPLIFF@SPLIFF@SPLIFF@@###################################@@@@@(((((((((((((((((((((((((%%%%%
+%%%%%(((((((((((((((@SPLIFF@SPLIFF@SPLIFF@SPLIFF@@###################################@@@@@(((((((((((((((((((((((((%%%%%
+%%%%%((((((((((EMBER(((((,,,,,,,,,,,,,,,,,,,,,,,,,@@@@@MOUTH&&&&&####################@@@@@(((((((((((((((((((((((((%%%%%
+%%%%%((((((((((EMBER(((((,,,,,,,,,,,,,,,,,,,,,,,,,@@@@@MOUTH&&&&&####################@@@@@(((((((((((((((((((((((((%%%%%
+%%%%%(((((((((((((((@SPLIFF@SPLIFF@SPLIFF@SPLIFF@@##############################/////@@@@@(((((((((((((((((((((((((%%%%%
+%%%%%(((((((((((((((@SPLIFF@SPLIFF@SPLIFF@SPLIFF@@##############################/////@@@@@(((((((((((((((((((((((((%%%%%
+%%%%%(((((((((((((((((((((((((((((((((((@@@@@##############################//////////@@@@@(((((((((((((((((((((((((%%%%%
+%%%%%(((((((((((((((((((((((((((((((((((@@@@@##############################//////////@@@@@(((((((((((((((((((((((((%%%%%
+%%%%%((((((((((((((((((((((((((((((((((((((((@@@@@@@@@@@@@@@@@@@@@@@@@///////////////@@@@@(((((((((((((((((((((((((%%%%%
+%%%%%((((((((((((((((((((((((((((((((((((((((@@@@@@@@@@@@@@@@@@@@@@@@@///////////////@@@@@(((((((((((((((((((((((((%%%%%
+%%%%%((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((@@@@@///////////////@@@@@(((((((((((((((((((((((((%%%%%
+%%%%%((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((@@@@@///////////////@@@@@(((((((((((((((((((((((((%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%@@@@@///////////////@@@@@%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%@@@@@///////////////@@@@@%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+ <~~~*/
+pragma solidity  >=0.8.0 <0.9.0;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "./interfaces/IRoleProvider.sol";
+import "./interfaces/ICollections.sol";
+
+contract MarketCollections is ICollections {
+
+  //*~~~> Roles for designated accessibility
+  bytes32 public constant PROXY_ROLE = keccak256("PROXY_ROLE"); 
+  bytes32 public constant DEV_ROLE = keccak256("DEV_ROLE"); 
+  string Mess = "DOES NOT HAVE ADMIN ROLE";
+  address public roleAdd;
+    
+    constructor(address _role) {
+      roleAdd = _role;
+    }
+    modifier hasAdmin(){
+      require(IRoleProvider(roleAdd).hasTheRole(PROXY_ROLE, msg.sender), Mess);
+      _;
+    }
+    modifier hasDevAdmin(){
+      require(IRoleProvider(roleAdd).hasTheRole(DEV_ROLE, msg.sender), Mess);
+      _;
+     }
+
+
+  //*~~~> Memory array of all listed Market Collections
+  mapping(address => bool) private addressToRestricted;
+  mapping(address => bool) private addressToOfferable;
+
+  //*~~~> Declaring event object structure for updated collection
+  event CollectionUpdated(
+    bool isNotTradable,
+    address indexed collectionContract
+  );
+
+  function setRoleAdd(address _role) public hasAdmin returns(bool){
+    roleAdd = _role;
+    return true;
+  }
+
+  /// @notice
+    //*~~~> OnlyOwner function to edit a new marketplace item
+  /// @dev 
+    /*~~~>
+      restricted: (true) if collection cannot trade;
+      nftContract: collection contract address;
+      <~~~*/
+  /// @return Bool
+  function editMarketplaceContract(
+    bool[] memory restricted,
+    address[] memory nftContract
+    ) external hasAdmin returns (bool) {
+    for (uint i; i<nftContract.length; i++) {
+      address contract1 = nftContract[i];
+      addressToRestricted[contract1] = restricted[i];
+      emit CollectionUpdated(restricted[i], contract1);
+    }
+    return true;
+  }
+
+  //*~~~> sets approved tokens for offers
+  function setTokenList(bool[] calldata _canOffer, address[] calldata _token) external hasAdmin returns (bool) {
+    for (uint i; i < _token.length; i++){
+      addressToOfferable[_token[i]] = _canOffer[i];
+    }
+    return true;
+  }
+
+  function canOfferToken(address token) external view returns(bool){
+    return addressToOfferable[token];
+  }
+  function canOfferToken(address[] memory tokens) external view returns(bool){
+    for(uint256 i = 0; i < tokens.length; i++) {
+      if (canOfferToken(tokens[i])) {
+        return true;
+      }
+    }
+    return false;  }
+
+  // checks if the collection is restricted from trading, returns false if not
+  function isRestricted(address nftContract) external view returns (bool) {
+    require(nftContract != address(0), "Trying to check zero address");
+    return addressToRestricted[nftContract];
+  }
+  function isRestricted(address[] memory nftContracts) external view returns (bool) {
+    for(uint256 i = 0; i < nftContracts.length; i++) {
+      if (isRestricted(nftContracts[i])) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  ///@notice
+  /*~~~> External ETH transfer forwarded to role provider contract <~~~*/
+  event FundsForwarded(uint value, address _from, address _to);
+  receive() external payable {
+    payable(roleAdd).transfer(msg.value);
+      emit FundsForwarded(msg.value, msg.sender, roleAdd);
+  }
+}
