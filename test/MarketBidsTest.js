aa940d459e4191fe4b6a3230c08f60a29abcbf12
phunkyjon.eth <83514596+StarKeyJON@users.noreply.github.com>
2022-03-03 21:16:58 -0500

    v2

--- test/MarketBidsTest.js
+++ test/MarketBidsTest.js
@@ -5,6 +5,7 @@ const { expect } = require("chai");
 const { solidity } = require("ethereum-waffle");
 const Web3 = require('web3');
 const { utils, BigNumber } = require("ethers");
+const { parseUnits, formatUnits } = require("ethers/lib/utils");
 // const toBN = Web3.utils.toBN;
 // use(solidity);
 
@@ -17,7 +18,7 @@ describe("MarketPlace Bids Contract Unit Test", function() {
     const [testDao, testDev, userAddress] = await ethers.getSigners();
 
     const RoleProvider = await ethers.getContractFactory("MarketRoleProvider");
-    const roleProvider = await RoleProvider.deploy("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266");
+    const roleProvider = await RoleProvider.deploy();
     await roleProvider.deployed();
     const roleProviderAddress = roleProvider.address;
     console.log("Role provider Address is: ", roleProviderAddress);
@@ -42,15 +43,16 @@ describe("MarketPlace Bids Contract Unit Test", function() {
 
     /// Test ERC20 token!! This is not used on mainnet!
     const PHAMToken = await ethers.getContractFactory("PhamToken");
-    const token = await PHAMToken.deploy('0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266');
+    const token = await PHAMToken.deploy(testDao.address);
     await token.deployed()
     const tokenAddress = token.address;
     console.log("PHAM Token Address: " + tokenAddress)
-    const amnt = await token.balanceOf('0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266');
-    console.log("Deployer address has a total of " + amnt.toNumber() + " PHAM Tokens.");
+    const amnt = await token.balanceOf(testDao.address);
+    const tokenamount = BigNumber.from(amnt);
+    console.log("Deployer address has a total of " + ethers.utils.formatUnits(tokenamount) + " PHAM Tokens.");
 
     const NFT = await ethers.getContractFactory("PhamNFTs");
-    const phamNft = await NFT.deploy('0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266', '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266', marketMintAddress, "https://ipfs.io/ipfs/");
+    const phamNft = await NFT.deploy(testDao.address, testDao.address, testDev.address, "https://ipfs.io/ipfs/", 20);
     await phamNft.deployed()
     const phamNftContractAddress = phamNft.address;
     console.log("PhamNFTs Contract Address: "+ phamNftContractAddress)
@@ -97,60 +99,72 @@ describe("MarketPlace Bids Contract Unit Test", function() {
     await roleProvider.setRoleAdd(roleProviderAddress);
     await roleProvider.setOwnerProxyAdd(ownerProxyAddress);
     await roleProvider.setPhunkyAdd(tokenAddress);
-    await roleProvider.setDevSigAddress(testDao.getAddress());
+    await roleProvider.setDevSigAddress(testDao.address);
     await roleProvider.setNftAdd(phamNftContractAddress);
     console.log("Initialized all the contract addresses to the Owner Proxy contract and assigned Contract_Role.")
-    await roleProvider.grantRole("0x77d72916e966418e6dc58a19999ae9934bef3f749f1547cde0a86e809f19c89b",ownerProxyAddress);
-    await ownerProxy.setProxyRole("0x51b355059847d158e68950419dbcd54fad00bdfd0634c2515a5c533288c7f0a2",testDao.getAddress())
-    await ownerProxy.setProxyRole("0x51b355059847d158e68950419dbcd54fad00bdfd0634c2515a5c533288c7f0a2",testDev.getAddress())
-    const address = await testDev.getAddress();
+
+    /// Dev multi sig will need to remain as DEFAULT_ADMIN
+    const address = testDev.address;
+    console.log(address)
+    await roleProvider.grantRole("0x77d72916e966418e6dc58a19999ae9934bef3f749f1547cde0a86e809f19c89b",address)
+
+    await roleProvider.grantRole("0x51b355059847d158e68950419dbcd54fad00bdfd0634c2515a5c533288c7f0a2",address)
+    
+    /// Assiging mint contract minter role
+    await phamNft.grantRole("0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6", marketMintAddress);
+
+    /// Setting the total amount of NFTs minted in the available NFTs array
+    const tokens = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19];
+    await marketMint.setNftTokenIds(tokens);
+    console.log("Nft ids set with: ", tokens)
+
+    const bal = await token.balanceOf(address)
+    console.log("PHAM token balance of testDao: ", formatUnits(BigNumber.from(bal)))
+       
     const balance = await ethers.provider.getBalance(address);
     const eth = ethers.utils.formatEther(balance);
     console.log(address, eth);
+
     await marketCollections.editMarketplaceContract([false],[phamNftContractAddress])
     
-    await marketMint.setNewRedemption(200, tokenAddress);
+    await marketMint.setNewRedemption(20, tokenAddress);
     await marketMint.fetchRedemptionTokens().then(async(ack) =>{
-      expect(await ack[0].redeemAmount.toNumber() === 200)
+      expect(await ack[0].redeemAmount.toNumber() === 20)
     })
     await marketCollections.setTokenList([true], [tokenAddress])
+    console.log("Token list set")
     await token.approve(marketMintAddress, 1000)
-    await marketMint.redeemForNft(1, 200, "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266")
-
-      await marketMint.redeemForNft(1, 200, "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266")
-
-        await marketMint.redeemForNft(1, 200, "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266")
-
-          await marketMint.redeemForNft(1, 200, "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266")
-
-            await marketMint.redeemForNft(1, 200, "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266")
+    console.log("Token approved")
+    await marketMint.redeemForNft(1)
+    await marketMint.redeemForNft(1)
+    await marketMint.redeemForNft(1)
+    await marketMint.redeemForNft(1)
+    await marketMint.redeemForNft(1)
+    await marketMint.redeemForNft(1)
+    await marketMint.redeemForNft(1)
+    await marketMint.redeemForNft(1)
+    await marketMint.redeemForNft(1)
+    await marketMint.redeemForNft(1)
+    await marketMint.redeemForNft(1)
+    await marketMint.redeemForNft(1)
+    await marketMint.redeemForNft(1)
+    await marketMint.redeemForNft(1)
+    await marketMint.redeemForNft(1)
+    await marketMint.redeemForNft(1)
+    await marketMint.redeemForNft(1)
+    await marketMint.redeemForNft(1)
+    await marketMint.redeemForNft(1)
+    await marketMint.redeemForNft(1)
 
     // Verifying the item struct and data is saved correctly
-    await marketMint.fetchNFTsCreated().then(async(res)=>{
-      expect(await res[0].itemId.toNumber() === 1);
-      expect(await res[0].creator === "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266");
-      expect(await res[0].itemId.toNumber() === 2);
-      expect(await res[0].creator === "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266");
-      expect(await res[0].itemId.toNumber() === 3);
-      expect(await res[0].creator === "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266");
-      expect(await res[0].itemId.toNumber() === 4);
-      expect(await res[0].creator === "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266");
-      expect(await res[0].itemId.toNumber() === 5);
-      expect(await res[0].creator === "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266");
-    });
-
-    await marketMint.fetchNFTsCreatedByAddress("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266").then(async(res)=>{
-      expect(await res[0].itemId.toNumber() === 1);
-      expect(await res[0].creator === "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266");
-      expect(await res[0].itemId.toNumber() === 2);
-      expect(await res[0].creator === "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266");
-      expect(await res[0].itemId.toNumber() === 3);
-      expect(await res[0].creator === "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266");
-      expect(await res[0].itemId.toNumber() === 4);
-      expect(await res[0].creator === "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266");
-      expect(await res[0].itemId.toNumber() === 5);
-      expect(await res[0].creator === "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266");
+    await marketMint.fetchNFTsCreated().then(res=>{
+      res.forEach(item=>{
+        console.log("item Id: ",item.itemId.toNumber())
+        console.log("token Id: ",item.tokenId.toNumber())
+        console.log("Contract address: ",item.contractAddress)
+      })
     })
+
     await rewardsController.fetchUserRewardTokens().then(async(res)=>{
       console.log(res[0].tokenId.toNumber())
       console.log(res[0].tokenAmount.toNumber())
@@ -170,7 +184,7 @@ describe("MarketPlace Bids Contract Unit Test", function() {
 
     // It should approve and list newly minted NFTs
     await phamNft.setApprovalForAll(marketAddress, true);
-    await market.listMktItem([false,false,false], [0,0,0], [0,1,2], [ethers.utils.parseUnits(".1","ether"),ethers.utils.parseUnits(".1","ether"),ethers.utils.parseUnits(".1","ether")], [phamNftContractAddress,phamNftContractAddress,phamNftContractAddress])
+    await market.listMktItem([false,false,false], [0,0,0], [0,1,2], [ethers.utils.parseUnits("10","ether"),ethers.utils.parseUnits("10","ether"),ethers.utils.parseUnits("10","ether")], [phamNftContractAddress,phamNftContractAddress,phamNftContractAddress])
     console.log("Item 1, 2 and 3 listed for sale")
     console.log("______________________")
 
@@ -193,11 +207,11 @@ describe("MarketPlace Bids Contract Unit Test", function() {
       
     })
     // It should sell the listed NFTs to test user 1
-    await market.connect(userAddress).buyMarketItems([1], { value: ethers.utils.parseUnits(".1","ether")})
+    await market.connect(userAddress).buyMarketItems([1], { value: ethers.utils.parseUnits("10","ether")})
     console.log("Item 1 sold to "+userAddress.address)
-    await market.connect(userAddress).buyMarketItems([2], { value: ethers.utils.parseUnits(".1","ether")})
+    await market.connect(userAddress).buyMarketItems([2], { value: ethers.utils.parseUnits("10","ether")})
     console.log("Item 2 sold to "+userAddress.address)
-    await market.connect(userAddress).buyMarketItems([3], { value: ethers.utils.parseUnits(".1","ether")})
+    await market.connect(userAddress).buyMarketItems([3], { value: ethers.utils.parseUnits("10","ether")})
     console.log("Item 3 sold to "+userAddress.address)
     
     console.log("______________________")
@@ -264,4 +278,58 @@ describe("MarketPlace Bids Contract Unit Test", function() {
       })
       
     })
+
+    const balance2 = await token.balanceOf(testDao.address);
+    const eth2 = ethers.utils.formatEther(balance2);
+    console.log(testDao.address, eth2);
+    await rewardsController.addDev(testDev.address);
+    
+    await marketMint.setNewRedemption(200, tokenAddress);
+    await marketMint.fetchRedemptionTokens().then(async(ack) =>{
+      expect(await ack[0].redeemAmount.toNumber() === 200)
+    })
+    await marketCollections.setTokenList([true], [tokenAddress])
+    await token.approve(marketMintAddress, 10000)
+
+    console.log("User Rewards holdings")
+    await rewardsController.fetchUserRewardTokens().then(async(res)=>{
+      console.log("tokenId: ",res[0].tokenId.toNumber())
+      console.log("tokenAmount: ",res[0].tokenAmount.toNumber())
+      console.log("tokenAddress: ", res[0].tokenAddress)
+    })
+    console.log("DEV Rewards holdings")
+    await rewardsController.fetchDevRewardTokens().then(async(res)=>{
+      console.log("tokenId: ",res[0].tokenId.toNumber())
+      console.log("tokenAmount: ",res[0].tokenAmount.toNumber())
+      console.log("tokenAddress: ", res[0].tokenAddress)
+    })
+    console.log("DAO Rewards holdings")
+    await rewardsController.fetchDaoRewardTokens().then(async(res)=>{
+      console.log("tokenId: ",res[0].tokenId.toNumber())
+      console.log("tokenAmount: ",res[0].tokenAmount.toNumber())
+      console.log("tokenAddress: ", res[0].tokenAddress)
+    })
+    await rewardsController.connect(testDao).claimDaoRewards();
+    const balance3 = await token.balanceOf(testDao.address);
+    const eth3 = ethers.utils.formatEther(balance3);
+    console.log(testDao.address, eth3);
+    console.log("DAO Rewards holdings after claim")
+    await rewardsController.fetchDaoRewardTokens().then(async(res)=>{
+      console.log("tokenId: ",res[0].tokenId.toNumber())
+      console.log("tokenAmount: ",res[0].tokenAmount.toNumber())
+      console.log("tokenAddress: ",res[0].tokenAddress)
+    })
+    const balance4 = await token.balanceOf(testDao.address);
+    const eth4 = ethers.utils.formatEther(balance4);
+    console.log(testDao.address, eth4);
+    await rewardsController.setClaimClock()
+    console.log("Claim Clock")
+
+    await rewardsController.fetchClaimTime().then(async(res)=>{
+      console.log("Aplha time: ", res.alpha.toNumber())
+      console.log("Delta time: ",res.delta.toNumber())
+      console.log("Omega time: ",res.omega.toNumber())
+      console.log("How many users: ", res.howManyUsers.toNumber())
+    })
+
 })})
