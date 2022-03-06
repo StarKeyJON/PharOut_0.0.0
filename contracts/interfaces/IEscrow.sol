afcd123ac1f0ffc2e53d979851a6ab3eb701e07c
phunkyjon.eth <83514596+StarKeyJON@users.noreply.github.com>
2022-03-05 22:30:21 -0500

    Update IEscrow.sol

--- contracts/interfaces/IEscrow.sol
+++ contracts/interfaces/IEscrow.sol
@@ -1,4 +1,5 @@
-pragma solidity 0.8.12;
+//*~~~> SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
 
 interface IOffers {
   function fetchOfferId(uint marketId) external returns(uint);
@@ -8,7 +9,6 @@ interface ITrades {
   function fetchTradeId(uint marketId) external returns(uint);
   function refundTrade(uint itemId, uint tradeId) external returns (bool);
 }
-
 interface IBids {
   function fetchBidId(uint marketId) external returns(uint);
   function refundBid(uint bidId) external returns (bool);
