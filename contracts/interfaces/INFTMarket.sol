36de81bbe39dac390ce77b17b767df72106bd6c2
phunkyjon.eth <83514596+StarKeyJON@users.noreply.github.com>
2022-03-05 22:30:36 -0500

    Update INFTMarket.sol

--- contracts/interfaces/INFTMarket.sol
+++ contracts/interfaces/INFTMarket.sol
@@ -1,5 +1,6 @@
-pragma solidity 0.8.12;
+//*~~~> SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
 
 interface INFTMarket { 
-    function transferNftForSale(address receiver, uint itemId) external;
+    function transferNftForSale(address receiver, uint itemId) external returns(bool);
 }
