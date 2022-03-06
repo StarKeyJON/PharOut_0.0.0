489202d4da269a57a1b607d14a1ea3b52b71d94c
phunkyjon.eth <83514596+StarKeyJON@users.noreply.github.com>
2022-03-05 22:30:50 -0500

    Update IRewardsController.sol

--- contracts/interfaces/IRewardsController.sol
+++ contracts/interfaces/IRewardsController.sol
@@ -1,11 +1,12 @@
-pragma solidity 0.8.12;
+//*~~~> SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
 
 interface IRewardsController {
   function createNftHodler(uint tokenId) external returns (bool);
   function depositERC20Rewards(uint amount, address tokenAddress) external returns(bool);
   function getFee() external view returns(uint);
   function setFee(uint _fee) external returns (bool);
-  function splitRewards(uint _split) external payable returns(bool);
+  function depositEthRewards(uint reward) external payable returns(bool);
   function createUser(address userAddress) external returns(bool);
   function setUser(bool canClaim, address userAddress) external returns(bool);
 }
