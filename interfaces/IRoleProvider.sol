f837c925e82b359f9be379081978e97daee46258
phunkyjon.eth <83514596+StarKeyJON@users.noreply.github.com>
2022-03-05 20:54:26 -0500

    Update IRoleProvider.sol

--- interfaces/IRoleProvider.sol
+++ interfaces/IRoleProvider.sol
@@ -1,9 +1,8 @@
-pragma solidity 0.8.12;
+//*~~~> SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
 
 interface IRoleProvider {
   function hasTheRole(bytes32 role, address _address) external returns(bool);
   function fetchAddress(bytes32 _var) external returns(address);
-
   function hasContractRole(address _address) external view returns(bool);
-  
 }
