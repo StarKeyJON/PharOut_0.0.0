ddb4eb93dd969dfbeaa66c9d3633fbb7e2815bfa
phunkyjon.eth <83514596+StarKeyJON@users.noreply.github.com>
2022-03-10 13:48:28 -0500

    Create PhamNFTs.sol

---
+++ tempMint/contracts/PhamNFTs.sol
@@ -0,0 +1,62 @@
+pragma solidity 0.8.7;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract PhamNFTs is ERC721, AccessControl {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIdCounter;
+
+    bytes32 public constant USER_OWNER_ROLE = keccak256("USER_OWNER");
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    uint256 tokenId;
+    uint256 supply;
+    string baseUri;
+
+    constructor(address userOwnerAddress, address controllerAddress, address minterAddress, string memory _baseUri, uint _supply) ERC721("pharOutNfts", "PHAROUT") {
+      _grantRole(DEFAULT_ADMIN_ROLE, controllerAddress);
+      _grantRole(USER_OWNER_ROLE, userOwnerAddress);
+      _grantRole(MINTER_ROLE, minterAddress);
+      baseUri = _baseUri;
+      supply = _supply;
+    }
+
+    function safeMint(address to, uint _tokenId) public payable onlyRole(MINTER_ROLE)  {
+      require(supply >= _tokenId, "Not enough left");
+      _safeMint(to, _tokenId);
+    }
+    
+    /*~~~> 
+      For setting the total supply of NFTs as more artwork comes in from the community;
+      Can only be increased;
+    <~~~*/
+    function setSupply(uint totalAmount) public onlyRole(USER_OWNER_ROLE) returns(bool){
+      require(totalAmount >= _tokenIdCounter.current());
+      supply = totalAmount;
+      return true;
+    }
+    
+    function setBaseUri(string memory _baseUri) public onlyRole(USER_OWNER_ROLE) {
+        baseUri = _baseUri;
+    }
+    
+    function tokenURI(uint256 _tokenId) public view virtual override returns (string memory) {
+        require(_exists(_tokenId), "ERC721Metadata: URI query for nonexistent token");
+        return string(abi.encodePacked(baseUri, Strings.toString(_tokenId), ".json"));
+    }
+
+    function withdraw() public onlyRole(USER_OWNER_ROLE) {
+        payable(msg.sender).transfer(address(this).balance);
+    }
+    
+    function supportsInterface(bytes4 interfaceId)
+    public
+    view
+    override(ERC721, AccessControl)
+    returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
